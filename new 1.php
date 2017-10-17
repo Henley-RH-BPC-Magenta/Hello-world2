a67d930186d82047ddd18c0f24df91651c89e15c
Henley-RH-BPC-Magenta <32098696+Henley-RH-BPC-Magenta@users.noreply.github.com>
2017-10-17 14:03:41 +0100

    PHP
    PHP for the task

---
+++ new 1.php
@@ -0,0 +1,24 @@
+<?php
+print "<h2>PHP is Fun!</h2>";
+print "Hello World!<br>";
+print "I'm about to learn PHP!";?>
+
+
+<!doctype html>
+
+<html>
+<body>
+
+<?PHP
+$txt1= "Learn PHP";
+$txt2= "at Bournemouth and Poole College";
+$x = 2;
+$y = 4;
+
+
+print "<h2>" .$txt1 "</h2>";
+print "<h2>" .txt2 "</h2>";
+?>
+
+</body>
+</html>
